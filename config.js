d78efa5b2584aba465b904657bfba9deda6f8d21
chrism755 <cmuchui534@gmail.com>
2026-06-16 22:10:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Fill in YOUR email and app password here
 
 module.exports = {
-  email: "your-email@gmail.com",        // Replace with your Gmail address
-  password: "your16characterapppassword" // Replace with your 16-char app password from Google
+  email: "your-nentlinkagencies254@gmail.com",        // Replace with your Gmail address
+  password: "iblk cwna kjup zlqd" // Replace with your 16-char app password from Google
 };
