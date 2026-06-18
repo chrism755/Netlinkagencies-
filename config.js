b79513d7ecf11ec255a1379e3821596a6d574602
chrism755 <cmuchui534@gmail.com>
2026-06-18 15:31:47 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Configuration file for Gmail credentials
-// Fill in YOUR email and app password here
-
-module.exports = {
-  email: "your-nentlinkagencies254@gmail.com",        // Replace with your Gmail address
-  password: "iblkcwnakjupzlqd" // Replace with your 16-char app password from Google
-};
