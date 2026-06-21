e753f55bfec1b7ea2b8d26a85632796949cd3ac7
chrism755 <cmuchui534@gmail.com>
2026-06-21 18:40:50 +0300

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -44,8 +44,8 @@ export default async function handler(req, res) {
 
   const emails = {
     activation_pending: renderEmail(
-      'Payment Received ⏳',
-      `Dear ${username}, your transaction ID is being processed. We will notify you once your account is activated.`,
+      'Test user ⏳',
+      `Dear ${username}, your  is being processed. We will notify you once your account is activated.`,
       replyFooter
     ),
 
